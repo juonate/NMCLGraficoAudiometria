74c242fb11d27e322e673613be53b1dae74d3be5
Juan <Juan@Juan-PC>
2015-09-09 18:25:30 -0300

    Ya estamos cerca!!!

--- src/java/com/noemalife/chile/servlet/Audiometria.java
+++ src/java/com/noemalife/chile/servlet/Audiometria.java
@@ -4,14 +4,13 @@
  */
 package com.noemalife.chile.servlet;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.RenderedImage;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -20,17 +19,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.AxisState;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.NumberTick;
 import org.jfree.chart.axis.NumberTickUnit;
-import org.jfree.chart.axis.TickType;
+import org.jfree.chart.plot.DatasetRenderingOrder;
+import org.jfree.chart.plot.SeriesRenderingOrder;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.RectangleEdge;
 import org.jfree.util.ShapeUtilities;
 
 /**
@@ -38,28 +34,57 @@ import org.jfree.util.ShapeUtilities;
  * @author Juan
  */
 public class Audiometria extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("image/png");
         ServletOutputStream os = resp.getOutputStream();
- 
-        XYDataset dataset = createDataSet();
+        String sVaod = "VAOD=[(150,45),(250,45),(500,45),(1000,10),(2000,25),(3000,20),(4000,),(6000,),(8000,)]";
+        String sVaoi = "VAOI=[(150,20),(250,25),(500,30),(1000,10),(2000,40),(3000,20),(4000,15),(6000,25),(8000,30)]";
+        String sVood = "VOOD=[(150,35),(250,40),(500,45),(1000,35),(2000,25),(3000,20),(4000,15),(6000,10),(8000,15)]";
+        String sVooi = "VOOI=[(150,40),(250,45),(500,20),(1000,35),(2000,65),(3000,30),(4000,5),(6000,10),(8000,15)]";
+        
+        XYSeries serieVaod = parseCoordinates(sVaod, "VAOD");
+        XYSeries serieVaoi = parseCoordinates(sVaoi, "VAOI");
+        XYSeries serieVood = parseCoordinates(sVood, "VOOD");
+        XYSeries serieVooi = parseCoordinates(sVooi, "VOOI");
+        XYSeries serieOculta = new XYSeries("Serie Oculta");
+        
+        serieOculta.add(1000,10);
+        serieOculta.add(2000,25);
+        
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        
+        
+        dataset.addSeries(serieVaod);
+        dataset.addSeries(serieVaoi);
+        dataset.addSeries(serieVood);
+        dataset.addSeries(serieVooi);
+        dataset.addSeries(serieOculta);
+        
+        
         JFreeChart chart = ChartFactory.createXYLineChart("Audiometria", "Frecuencia", "Intensidad", dataset);
-        
+
         XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.LIGHT_GRAY);
+        plot.setSeriesRenderingOrder(SeriesRenderingOrder.FORWARD );
         
-//        plot.getRenderer().setSeriesStroke(0, new BasicStroke(2.0f));
+
         
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+//        renderer.setSeriesLinesVisible(0, true);
+//        renderer.setSeriesShapesVisible(0, false);
         renderer.setSeriesLinesVisible(0, true);
         renderer.setSeriesShapesVisible(0, true);
         renderer.setSeriesLinesVisible(1, true);
         renderer.setSeriesShapesVisible(1, true);
         renderer.setSeriesLinesVisible(2, true);
-        renderer.setSeriesShapesVisible(2, true); 
+        renderer.setSeriesShapesVisible(2, true);
         renderer.setSeriesLinesVisible(3, true);
-        renderer.setSeriesShapesVisible(3, true); 
+        renderer.setSeriesShapesVisible(3, true);
+        renderer.setSeriesLinesVisible(4, true);
+        renderer.setSeriesShapesVisible(4, true);
+//        renderer.setSeriesPaint(0, Color.LIGHT_GRAY);
+//        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
         renderer.setSeriesShape(0, ShapeUtilities.createDiamond(3));
         renderer.setSeriesPaint(0, Color.RED);
         renderer.setSeriesShape(1, new Ellipse2D.Double(-3.0, -3.0, 6.0, 6.0));
@@ -68,81 +93,46 @@ public class Audiometria extends HttpServlet {
         renderer.setSeriesPaint(2, Color.RED);
         renderer.setSeriesShape(3, new Ellipse2D.Double(-3.0, -3.0, 6.0, 6.0));
         renderer.setSeriesPaint(3, Color.BLUE);
+        renderer.setSeriesPaint(4, Color.LIGHT_GRAY);
+        renderer.setSeriesStroke(4, new BasicStroke(2.0f));
+        
         plot.setRenderer(renderer);
 
 //        renderer.setSeriesVisibleInLegend(0, false);
-        
+
         //Configuración eje Y
         plot.getRangeAxis().setInverted(true);
-        plot.getRangeAxis().setRange(-10,110);
-        NumberAxis rangeAxis = (NumberAxis)plot.getRangeAxis();
+        plot.getRangeAxis().setRange(-10, 110);
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         rangeAxis.setTickUnit(new NumberTickUnit(10));
-        
+
         //Configuración eje X
-        plot.getDomainAxis().setRange(0, 8000);
-        NumberAxis domainAxis = new NumberAxis(plot.getDomainAxis().getLabel()){
+        NumberAxis axis = (NumberAxis)plot.getDomainAxis();
+        axis.setRange(0, 8000);
 
-            @Override
-            public List refreshTicks(Graphics2D g2, AxisState state, Rectangle2D dataArea, RectangleEdge edge) {
-                List  allTicks = super.refreshTicks(g2, state, dataArea, edge);
-                List myTicks = new ArrayList();
-                
-                for(Object tick : allTicks){
-                    NumberTick numberTick = (NumberTick) tick;
-                    if(TickType.MAJOR.equals(numberTick.getTickType())&& (numberTick.getValue()% 50 !=0)){
-                        myTicks.add(new NumberTick(TickType.MINOR, numberTick.getValue(), "", numberTick.getTextAnchor(),numberTick.getRotationAnchor(),numberTick.getAngle()));
-                        continue;
-                    }
-                    myTicks.add(tick);
-                }
-                return myTicks;
-            }
-        };
-        plot.setDomainAxis(domainAxis);
         
 
-        RenderedImage imagenGrafico = chart.createBufferedImage(1000, 300);
+
+        RenderedImage imagenGrafico = chart.createBufferedImage(800, 300);
         ImageIO.write(imagenGrafico, "png", os);
         os.flush();
         os.close();
     }
-
-    public XYDataset createDataSet() {
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        XYSeries series1 = new XYSeries("VAOD");
-        XYSeries series2 = new XYSeries("VAOI");
-        XYSeries series3 = new XYSeries("VOOD");
-        XYSeries series4 = new XYSeries("VOOI");
-
-        series1.add(125, 15);
-        series1.add(250, 20);
-        series1.add(500, 30);
-        series1.add(1000, 20);
-        series1.add(2000, 40);
-
-        series2.add(125, 20);
-        series2.add(250, 45);
-        series2.add(500, 30);
-        series2.add(1000, 55);
-        series2.add(2000, 35);
-
-        series3.add(125, 15);
-        series3.add(250, 20);
-        series3.add(500, 15);
-        series3.add(1000, 30);
-        series3.add(2000, 35);
+    
+    public XYSeries parseCoordinates(String input, String label) {
+        XYSeries returnSerie = new XYSeries(label);
+        Pattern pattern = Pattern.compile("(\\d+),(-?\\d+\\*?|\\d+\\*?)");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            int x = Integer.parseInt(matcher.group(1));
+            int y = Integer.parseInt(matcher.group(2));
+            returnSerie.add(x, y);
+        }
+        return returnSerie;
+    }
+    
+    public XYSeries crearSerieOculta(String x, String Y){
         
-        series4.add(125, 25);
-        series4.add(250, 30);
-        series4.add(500, 35);
-        series4.add(1000, 10);
-        series4.add(2000, 10);
-
-        dataset.addSeries(series1);
-        dataset.addSeries(series2);
-        dataset.addSeries(series3);
-        dataset.addSeries(series4);
-
-        return dataset;
+        return null;
     }
 }
